9fe3332f8b73240a695c86003bd0a6735aefc0d8
fluxfw <fluxfw@bluewin.ch>
2024-01-09 14:57:30 +0100

    Removed `cssToStyleSheet.mjs`

--- src/cssToStyleSheet.mjs
+++
@@ -1,9 +0,0 @@
-/**
- * @param {string} css
- * @returns {Promise<CSSStyleSheet>}
- */
-export async function cssToStyleSheet(css) {
-    const style_sheet = new CSSStyleSheet();
-    style_sheet.replaceSync(css);
-    return style_sheet;
-}
